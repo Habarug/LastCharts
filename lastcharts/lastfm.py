7e9f7de657e215c6089afec8e01145d1ea94ecfe
Habarug <habarug@gmail.com>
2024-05-18 22:27:58 +0200

    Get recent tracks working

--- lastcharts/lastfm.py
+++ lastcharts/lastfm.py
@@ -11,11 +11,36 @@ class LastFM:
 
         self.payload_base = {"api_key": API_key, "format": "json"}
 
-    def lastfm_get(self, method):
-        payload = self.payload_base | {"method": method}
+    def lastfm_get(self, payload):
+        payload = self.payload_base | payload
 
         try:
             response = requests.get(self.URL_base, headers=self.headers, params=payload)
             return response
         except Exception as e:
             raise e
+
+    def get_recent_tracks(
+        self, user: str = None, page: int = 1, limit: int = 200, start: int = 0
+    ):
+        """Get recent tracks for user
+
+        Args:
+            user    : LastFM username. If None, defaults to config
+            page    : Page number (1 indexed). Default = 1
+            limit   : Number of results per page. Default = 200 = Max
+            start   : Beginning timestamp of range. Default = 0 (1970-01-01)
+        """
+
+        if user is None:
+            user = self.headers["user-agent"]
+
+        return self.lastfm_get(
+            {
+                "method": "user.getRecentTracks",
+                "user": user,
+                "page": page,
+                "limit": limit,
+                "from": start,
+            }
+        )
