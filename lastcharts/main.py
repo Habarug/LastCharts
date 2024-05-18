e2fa27a3e14022272c8cc7a458cf2d166efb8477
Habarug <habarug@gmail.com>
2024-05-19 00:58:04 +0200

    Start making stacked bar chart, but not quite working

--- lastcharts/main.py
+++ lastcharts/main.py
@@ -2,6 +2,7 @@ import os
 import urllib
 
 import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 import pandas as pd
 import pyjson5
 
@@ -46,6 +47,71 @@ class LastCharts:
             freq="d",
         )
 
+    def plot_stacked_bar_plot(self, nArtists=15, artLimitCoefficient=0.05):
+        sizeX = 20
+        sizeY = 8
+        plt.rcParams["figure.figsize"] = (sizeX, sizeY)
+        width = 0.9
+        cover = 0.85
+
+        artLimit = (
+            artLimitCoefficient
+            * self.df[self.df["artist"] == self.topArtists[0]].shape[0]
+        )
+        scale = (
+            self.df[self.df["artist"] == self.topArtists[0]].shape[0]
+            / nArtists
+            * sizeX
+            / sizeY
+        )
+
+        fig, ax = plt.subplots()
+
+        for artist in self.topArtists[0:nArtists]:
+            filterArtist = self.df[(self.df["artist"] == artist)]
+            albums = filterArtist["album"].unique()
+
+            albumsCount = []
+
+            for album in albums:
+                albumScrobbles = filterArtist[filterArtist["album"] == album]
+                albumsCount.append(albumScrobbles.shape[0])
+
+            albums = [y for x, y in sorted(zip(albumsCount, albums), reverse=True)]
+            albumsCount = [x for x, y in sorted(zip(albumsCount, albums), reverse=True)]
+
+            bottom = 0
+            plt.gca().set_prop_cycle(None)
+
+            for idx, count in enumerate(albumsCount):
+                bp = plt.bar(artist, count, width, bottom=bottom)
+
+                patch = bp.patches
+                (x, y) = patch[0].get_xy()
+
+                if count >= artLimit:
+                    size = min(count * 0.95, cover * scale)
+                    extent = [
+                        x + width / 2 - size / (2 * scale),
+                        x + width / 2 + size / (2 * scale),
+                        bottom + count / 2 - size / 2,
+                        bottom + count / 2 + size / 2,
+                    ]
+                    img = self._get_cover(artist, albums[idx])
+                    ax.set_autoscale_on(False)
+                    if img:
+                        plt.imshow(img, extent=extent, aspect="auto", zorder=3)
+
+            plt.xticks(rotation=45, fontsize=self.__font_size_ticks)
+            plt.yticks(fontsize=self.__font_size_ticks)
+            plt.ylabel("Scrobble count", fontsize=self.__font_size_axis_labels)
+            plt.xlim(-0.5, nArtists - 1.5)
+            plt.ylim(
+                0, self.scrData[self.scrData["artist"] == self.topArtists[0]].shape[0]
+            )
+            fig.patch.set_facecolor("xkcd:white")
+            plt.tight_layout()
+
     def _get_cover(self, artist, album, force=0):
         if not os.path.exists(self.COVER_dir):
             os.mkdir(self.COVER_dir)
@@ -64,8 +130,12 @@ class LastCharts:
             .dropna()
             .iloc[0]
         )
-        urllib.request.urlretrieve(url, savePath)
-        return mpimg.imread(savePath)
+        try:
+            urllib.request.urlretrieve(url, savePath)
+            return mpimg.imread(savePath)
+        except Exception:
+            print(f"Cover not found for {artist} {album}")
+            return None
 
 
 def main():
