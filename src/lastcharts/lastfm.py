d46a392c25d3a64c6f293ba197bbb9df23fa26ca
Habarug <habarug@gmail.com>
2024-12-16 21:03:04 +0100

    Add method to get album info (needs parsing)

--- src/lastcharts/lastfm.py
+++ src/lastcharts/lastfm.py
@@ -18,6 +18,23 @@ class LastFM:
 
         self.payload_base = {"api_key": API_key, "format": "json"}
 
+    def get_album_info(self, artist: str, album: str):
+        """Get info on a specific album, return Dataframe
+
+        Args:
+            artist  : Artist
+            album   : Album
+
+        Return:
+            Dataframe with columns : artist (str), album (str), releasedata (datetime), image, listeners, playcount, toptag
+        """
+
+        r = self._lastfm_get(
+            {"method": "album.getInfo", "artist": artist, "album": album}
+        )
+
+        return r
+
     def _lastfm_get(self, payload):
         payload = self.payload_base | payload
 
