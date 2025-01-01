25c5f14ee68cc22b3f01eb0aade020c361e23922
Habarug <habarug@gmail.com>
2025-01-01 10:48:38 +0100

    Drop df duplicates

--- src/lastcharts/lastfm.py
+++ src/lastcharts/lastfm.py
@@ -166,7 +166,9 @@ class LastFM:
                 df = pd.concat([df_new, df])
             else:
                 df = df_new
-            df.to_csv(path, index=False)
+
+        df.drop_duplicates()
+        df.to_csv(path, index=False)
 
         print("Scrobbles loaded")
 
